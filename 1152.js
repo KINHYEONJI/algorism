b1466fb8365f198cef2ab96f372af80afa3c7147
KINHYEONJI <hyeonji0718@gmail.com>
2022-12-07 23:15:53 +0900

    feat: 1152 solved
    - trim() : 문자열 시작과 끝에 공백을 삭제해주는 함수

---
+++ 1152.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+// let fs = require("fs");
+// let input = fs.readFileSync("./input.txt").toString().trim().split(" ");
+
+console.log(input[0] === "" ? 0 : input.length);
